42251e6b4180786c65a04caeb6cca049b8a98a87
shaoTimbleWolf <18624229408@163.com>
2019-06-28 11:01:37 +0800

    03-userservice

---
+++ sp03-userservice/src/main/java/com/tedu/sp03/Sp03UserserviceApplication.java
@@ -0,0 +1,14 @@
+package com.tedu.sp03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class Sp03UserserviceApplication {
+////fsdkjfnsdjkfn
+	public static void main(String[] args) {
+		SpringApplication.run(Sp03UserserviceApplication.class, args);
+	}
+
+}
